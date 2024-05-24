f8864a42fe17cfe5e71209e35ee81d37f3f8e9f6
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2024-05-24 15:14:33 -0300

    utilizandoZod

--- src/Controllers/habitsController.ts
+++ src/Controllers/habitsController.ts
@@ -1,4 +1,5 @@
 import { type Request, type Response } from "express"
+import { z } from "zod"
 
 import { habitModel } from "../Models/habitModel"
 export class HabitsController {
@@ -6,23 +7,31 @@ export class HabitsController {
     request: Request,
     response: Response,
   ): Promise<Response> => {
+    const schema = z.object({
+      name: z.string(), // tipamos o name como uma string utilizando Zod, tal qual utilizamos com YUP
+    })
+
     const { name } = request.body
 
-    const findHabit = await habitModel.findOne({ name }) // Vamos procurar se tem algum nome igual
+    const habit = schema.safeParse({
+      // conforme documentaçõ do Zod, precisamos usar o safeParse para trazer mensagens mais detalhadas do problema
+      name,
+    })
+
+    if (!habit.success) {
+      // aqui caso não seja trazido true no succcess do zod, ele manda essa mensagem de erro
+      return response.status(400).json({ message: "error on validation" }) // caso de tudo certo ele continua a aplicação abaixo
+    }
+
+    const findHabit = await habitModel.findOne({ name: habit.data.name }) // Vamos procurar se tem algum nome igual
 
     if (findHabit) {
       // Se tiver nome igual, retorna a mensagem
       return response.status(400).json({ message: "Habit already exists" })
-    }
+    } // se não tiver nome igual ele segue o baile
 
-    // Verificando se 'name' foi fornecido e é uma string
-    if (typeof name !== "string") {
-      return response
-        .status(400)
-        .json({ error: "Name is required and must be a string" })
-    }
     const newHabit = await habitModel.create({
-      name,
+      name: habit.data.name,
       completedDates: [],
     })
 
